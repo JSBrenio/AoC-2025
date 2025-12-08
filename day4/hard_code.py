e708862b0b20e0b5fbaca3cd43de7235a6c25504
Jsbrenio <brenio.jeremiah.2021@gmail.com>
2025-12-07 18:46:59 -0800

    Complete Hard Problem Day 4

---
+++ day4/hard_code.py
@@ -0,0 +1,47 @@
+from pprint import pprint
+from copy import deepcopy
+# NW N NE
+# W    E
+# SW S SE
+# -1,-1 | -1,0 | -1,1
+#  0,-1 |      | 0,1
+#  1,-1 |  1,0 | 1,1
+dirs = [[-1,-1], [-1,0], [-1,1], [0,-1], [0,1], [1,-1], [1,0], [1,1]]
+grid = []
+result = 0
+with open('input.txt', 'r') as file:
+    for line in file:
+        line = line.strip()
+        # print(line)
+        grid.append([char for char in line])
+
+# pprint(grid)
+# print()
+threshold = 4
+newGrid = deepcopy(grid)
+removed = True
+while removed:
+    removed = False
+    grid = deepcopy(newGrid)
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            count = 0
+            if grid[row][col] == '@':
+                for (x, y) in dirs:
+                    try:
+                        nR = row + x
+                        nC = col + y
+                        if nR < 0 or nC < 0:
+                            continue
+                        if grid[nR][nC] == '@':
+                            # print(f"({nR}, {nC})")
+                            count += 1
+                    except:
+                        continue
+                # print(f"({row}, {col}): {count}")
+                if count < threshold:
+                    newGrid[row][col] = 'x'
+                    removed = True
+                    result += 1
+    # pprint(newGrid)
+print(result)
